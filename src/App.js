3bac9c2c8a38a6bc377b31249ec51ffdcf659434
DMendozaH <dani47052@gmail.com>
2023-11-29 13:23:55 -0500

    da
    ggg

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> HOLA MUNDO
+          Edit <code>src/App.js</code> HOLA MUNDO rrrrrrrrrrrrrrrrrrrrrrrrr
         </p>
         <a
           className="App-link"
