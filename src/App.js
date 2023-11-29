2a8bd1b814c0c55704c750c83a613626c731d820
DMendozaH <dani47052@gmail.com>
2023-11-29 13:23:08 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> HOLA MUNDO ASDFADFADFADFSA DFADFD
+          Edit <code>src/App.js</code> holaaaaaaa mundooooo
         </p>
         <a
           className="App-link"
